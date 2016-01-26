8c0a2e89488ecd3267ad853c3e957f1f89cb5a93
Pawel Kadluczka <moozzyk@gmail.com>
2016-01-25 17:02:08 -0800

    Adding AppOffline

---
+++ src/AppOffline/Startup.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNet.Builder;
+using Microsoft.AspNet.Hosting;
+using Microsoft.AspNet.Http;
+
+namespace AppOffline
+{
+    public class Startup
+    {
+        public void Configure(IApplicationBuilder app)
+        {
+            app.UseIISPlatformHandler();
+
+            app.Run(async context =>
+            {
+                await context.Response.WriteAsync("Maintentance");
+            });
+        }
+
+        public static void Main(string[] args)
+        {
+            var host = new WebHostBuilder()
+                .UseDefaultConfiguration(args)
+                .UseIISPlatformHandlerUrl()
+                .UseServer("Microsoft.AspNet.Server.Kestrel")
+                .UseStartup<Startup>()
+                .Build();
+
+            host.Run();
+        }
+    }
+}
